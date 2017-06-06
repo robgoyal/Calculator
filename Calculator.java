cc17de8fe17d68fee07869c50a628bc8144c8688
Robin Goyal <goyal.rob@gmail.com>
2017-06-06 15:13:04 -0400

    Cleaned code and updated ReadMe

--- Calculator.java
+++ Calculator.java
@@ -12,6 +12,7 @@ import java.util.*;
 
 public class Calculator extends JFrame implements ActionListener{
 
+    // Instance variables
     private JTextField display = new JTextField(17);
     private JTextField resultDisplay = new JTextField(17);
     private double result = 0;
@@ -30,13 +31,17 @@ public class Calculator extends JFrame implements ActionListener{
         setResizable(false);
         setVisible(true);
         
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.PAGE_AXIS));
-        //JTextField display = new JTextField(17);
+        // Initialize display fields
+        display = new JTextField(17);
+        resultDisplay = new JTextField(17);
+
+        // Text field Font
+        Font displayFont = display.getFont().deriveFont(Font.PLAIN, 25f);
+
+        // Text Field parameters
         display.setEditable(false);
         display.setText("");
         display.setHorizontalAlignment(JTextField.RIGHT);
-        Font displayFont = display.getFont().deriveFont(Font.PLAIN, 25f);
         display.setFont(displayFont);
 
         resultDisplay.setEditable(false);
@@ -44,6 +49,9 @@ public class Calculator extends JFrame implements ActionListener{
         resultDisplay.setHorizontalAlignment(JTextField.RIGHT);
         resultDisplay.setFont(displayFont);
 
+        // Add text fields to panel
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.PAGE_AXIS));
         topPanel.add(display);
         topPanel.add(resultDisplay);
 
@@ -76,20 +84,29 @@ public class Calculator extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof JButton) {
+
+            // Clicked button text
             String text = e.getActionCommand();
-            //display.setText(text);
 
             switch(text) {
+
+                // Clear text fields and total
                 case "Clear":
                     display.setText("");
                     resultDisplay.setText("");
                     result = 0;
                     break;
+
+                // Close application
                 case "Close":
                     System.exit(0);
+
+                // Include previous result in current expression
                 case "ANS":
                     display.setText(display.getText() + result);
                     break;
+
+                // Space text around operations for easy split
                 case "+":
                 case "-":
                 case "/":
@@ -98,63 +115,87 @@ public class Calculator extends JFrame implements ActionListener{
                 case ")":
                     display.setText(display.getText() + " " + text + " ");
                     break;
-                case "=":
-                    Stack<Double> values = new Stack<Double>();
-                    Stack<String> ops = new Stack<String>();
 
+                // Evaluate current expression
+                case "=":
+
+                    // Split string and trim whitespace to store in Queue
                     String[] expression = display.getText().trim().split("\\s+");
                     Queue<String> q = new LinkedList<>();
                     q.addAll(Arrays.asList(expression));
 
-                    System.out.println(expression);
-                    System.out.println(q);
+                    // Hold values and operations
+                    Stack<Double> values = new Stack<Double>();
+                    Stack<String> ops = new Stack<String>();
 
+                    // Iterate through queue till empty
                     while(!q.isEmpty()) {
+
+                        // Get queue token
                         String token = q.poll();
+
+                        // Ignore leading parenthesis
                         if (token.equals("(")) continue;
+
+                        // Push operand
                         else if (token.equals("+")) ops.push(token);
                         else if (token.equals("*")) ops.push(token);
                         else if (token.equals("-")) ops.push(token);
                         else if (token.equals("/")) ops.push(token);
+
+                        // Evaluate innermost expression 
                         else if (token.equals(")")) {
                             values.push(calculate(ops, values));
                         }
+
+                        // Push value
                         else {
                             values.push(Double.parseDouble(token));
                         }
                     }
+
+                    // Evaluate current values on stack if user didnt 
+                    // include one set of parenthesis
                     if (values.size() >= 2) {
                         result = calculate(ops, values);
                     }
                     else {
                         result = values.pop();
                     }
-
+    
+                    // Update expression display text field
                     display.setText("");
+                    // Display result in result text field
                     resultDisplay.setText(Double.toString(result));
                     break;
+
                 default:
                     display.setText(display.getText() + text);
-        // END CASE
             }
         }
     }
 
+    // Perform arithmetic operation on two values
     private double calculate(Stack<String> ops, Stack<Double> values) {
+
+        // Pop first value and operation
         String op = ops.pop();
         Double val = values.pop();
 
+        // Pop another value and perform operation
         if (op.equals("+")) val = values.pop() + val;
         else if (op.equals("-")) val = values.pop() - val;
         else if (op.equals("*")) val = values.pop() * val;
         else if (op.equals("/")) {
-            double val2 = values.pop();
-            if (val2 == 0.0) {
+
+            // Check for division by zero
+            if (val == 0.0) {
                 System.out.println("DIVISION BY ZERO");
                 System.exit(0);
             }
-            else val = val2 / val;
+            else val = values.pop() / val;
         }
+
         return val;
     }
 
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,3 @@
 # Calculator 
 
-A basic calculator app created in Java
+A basic calculator app created in Java that uses Djikstra's Two Stack algorithm to evaluate expressions. There isn't a ton of error checking with the app. It requires to try their best to include brackets within the expression. 
